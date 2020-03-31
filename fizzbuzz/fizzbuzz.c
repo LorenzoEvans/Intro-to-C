110b5cb47d8cfe4ab898a5f348190197ed33c8a2
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-03-30 18:58:07 -0700

    This cannot be that much harder just because it's C

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -2,30 +2,36 @@
 
 /*
     Your fizzbuzz implementation should initialize a counter, then
-    iterate n times. If the ith iteration is divisible by 3, print
-    "Fizz". If it's divisible by 5, print "Buzz". If it's divisible
-    by both 3 and 5, print "FizzBuzz". Increment the counter variable
-    every time that nothing gets printed and return the counter.
+    iterate n times. 
+    If the ith iteration is divisible by 3, print "Fizz". 
+    If it's divisible by 5, print "Buzz". 
+    If it's divisible by both 3 and 5, print "FizzBuzz". 
+    Increment the counter variable every time that nothing gets printed and return the counter.
     Don't forget to include newlines '\n' in your printf statements!
 */
 int fizzbuzz(int n)
 {
   int counter = 0;
-  
-  for (counter; counter < n; counter++) {
-    if (counter % 3 == 0 && counter % 5 == 0) {
+  // if ( n == 2 || 1) {
+      // return counter;
+  // }
+  while (counter < n) {
+    printf("%i\n", counter);
+    if (counter % 5 == 0 && counter % 3 == 0) {
       printf("FizzBuzz\n");
-      return counter;
     }
     else if (counter % 3 == 0) {
       printf("Fizz\n");
-      return counter;
+    }
+    else if (counter % 5 == 0) {
+      printf("Buzz\n");
     }
     else {
-      printf("Buzz\n");
       return counter;
     }
+    counter++;
   }
+  return 0;
 }
 
 #ifndef TESTING
